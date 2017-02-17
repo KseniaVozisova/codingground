a34b828ccd1d955438709d5d5e9d7510401032b8
KseniaVozisova <kseniya.vozisova@gmail.com>
2017-02-17 09:34:26 +0200

    Created by Coding Ground Client

---
+++ New Project-20170217/lab_01_01.py
@@ -0,0 +1,19 @@
+# lab_01_01 program in Python
+
+a = 15
+b = 7
+print("a = ", a)
+print("a = ", b)
+print("a + b = ", a+b)
+print("a - b = ", a-b)
+print("a * b = ", a*b)
+print("a / b - ", a/b)
+print("a // b = ", a // b) #деление с коруглением вниз до целого
+print("a % b = ", a % b) #остаток от деления
+print("a ** b = ", a**b) #возведение в степень
+
+
+# int k float
+a1 = float(a)
+print("a1 = ", a1)
+print("\n\n")
